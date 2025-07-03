b25f2541e9ad893fed112b2e9f36305fc47f4e80
lxy <123@123.com>
2025-07-03 17:33:41 +0800

    --

--- src/main/java/org/agent/MedAgent/agent/ChatMemorySingle.java
+++ src/main/java/org/agent/MedAgent/agent/ChatMemorySingle.java
@@ -1,17 +1,12 @@
 package org.agent.MedAgent.agent;
 
-import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import dev.langchain4j.memory.chat.ChatMemoryProvider;
-
-import java.util.List;
 
 @Configuration
-public class ChatMemoryTool {
+public class ChatMemorySingle {
 
     //增加聊天记忆，并在AiService添加注解value
     @Bean
